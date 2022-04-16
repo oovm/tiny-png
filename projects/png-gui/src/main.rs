abf77e017354e45220309fcc4ddbc131293e3e8f
Aster <galaster@foxmail.com>
2022-04-16 17:55:52 +0800

    🔮 Support ETA

--- projects/png-gui/src/main.rs
+++ projects/png-gui/src/main.rs
@@ -1,14 +1,34 @@
+use fs::write;
 use std::fs;
+
+use bytesize::ByteSize;
 use oxipng::{optimize_from_memory, Options};
 
 mod errors;
 
+
 pub fn main() {
-    let png = include_bytes!("../iphone.test.png");
-    let mut opt = Options {
+    let before = include_bytes!("../iphone.test.png");
+    let mut opts = Options {
         ..Options::default()
     };
-    let out = optimize_from_memory(png, &opt).unwrap();
-    fs::write("out.test.png", &out).unwrap();
+    let after = optimize_from_memory(before, &opts).unwrap();
+    println!("before: {}", ByteSize::b(before.len() as u64));
+    println!("after: {}", ByteSize::b(after.len() as u64));
+    println!("Reduce {:+.2}%", calc_reduce(before, &after));
+    if is_fully_optimized(before.len(), after.len(), &opts) {} else {
+        write("out.test.png", &after).unwrap();
+    }
+}
 
+
+fn is_fully_optimized(original_size: usize, optimized_size: usize, opts: &Options) -> bool {
+    original_size <= optimized_size && !opts.force && opts.interlace.is_none()
+}
+
+
+pub fn calc_reduce(before: &[u8], after: &[u8]) -> f64 {
+    let before = before.len() as f64;
+    let after = after.len() as f64;
+    (before - after) / -before
 }
